f3559366aeb1321447bc6810c3c0cfce136105aa
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2020-03-08 22:57:28 +0800

    Initial Commit

---
+++ src/main/java/com/marco/shop/constant/Category.java
@@ -0,0 +1,15 @@
+package com.marco.shop.constant;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Category {
+
+    public static final List<String> FOOD = Arrays.asList("potato chips");
+
+    public static final List<String> CLOTHING = Arrays.asList("shirt");
+
+    public static final List<String> STATIONARY = Arrays.asList("pencil", "book");
+
+}
